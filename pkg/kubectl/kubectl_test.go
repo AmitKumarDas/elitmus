7e7c8f1f25c4e31443024d910a661a8400ae275c
AmitKumarDas <amit.das@openebs.io>
2018-05-10 11:18:20 +0530

    add unit test cases to pkg/kubectl
    Motivation:
    Trying to make a slow but steady progress on UT
    as making huge changes will stall the progress.
    
    Some of these changes might involve changing the
    design e.g. adding one or more interfaces & fake
    structures. However, there are none in this commit.
    
    Signed-off-by: AmitKumarDas <amit.das@openebs.io>

---
+++ pkg/kubectl/kubectl_test.go
@@ -0,0 +1,71 @@
+/*
+Copyright 2018 The OpenEBS Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubectl
+
+import (
+	"testing"
+)
+
+func TestKubeCtlArgs(t *testing.T) {
+	tests := map[string]struct {
+		args      []string
+		namespace string
+		context   string
+		labels    string
+		expected  []string
+		isEmpty   bool
+	}{
+		"kubectl args - positive test case": {
+			args:      []string{"kubectl", "get", "po", "my-pod"},
+			namespace: "litmus",
+			context:   "",
+			labels:    "name=my-pod",
+			expected:  []string{"kubectl", "get", "po", "my-pod", "--namespace=litmus", "--selector=name=my-pod"},
+			isEmpty:   false,
+		},
+		"kubectl args - negative test case - empty": {
+			args:      []string{},
+			namespace: "",
+			context:   "",
+			labels:    "",
+			expected:  []string{},
+			isEmpty:   true,
+		},
+		"kubectl args - negative test case - nil": {
+			args:      nil,
+			namespace: "",
+			context:   "",
+			labels:    "",
+			expected:  nil,
+			isEmpty:   true,
+		},
+	}
+
+	for name, mock := range tests {
+		t.Run(name, func(t *testing.T) {
+			ops := kubectlArgs(mock.args, mock.namespace, mock.context, mock.labels)
+
+			if len(ops) == 0 && !mock.isEmpty {
+				t.Fatalf("failed to execute kubectl args: expected 'non nil output': actual 'nil output'")
+			}
+
+			if len(ops) != len(mock.expected) {
+				t.Fatalf("failed to execute kubectl args: expected output '%s': actual output '%s'", mock.expected, ops)
+			}
+		})
+	}
+}
